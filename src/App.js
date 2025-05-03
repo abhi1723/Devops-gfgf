beff141af1d13bb84e812fa3762e386ccd77956a
Kumar Abhishek <abhishek.kumar@eminds.ai>
2025-05-03 17:39:38 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   }
   return (
     <div>
-      Hello World!
+      Hello WorldLatest!
       {/* <Parent parentName = "Sumit" childName = "Raman"/>
       <Parent parentName = "Amit" childName = "Rahul"/>
       <Parent parentName = "Amit" childName = "Rahul"/> */}
